bfb5f160f49c6553c16c8308e4578720c9796987
Yuvasri <160617670+YuvasriSingaravelou@users.noreply.github.com>
2024-07-05 09:34:13 +0530

    My code

---
+++ For loop.py
@@ -0,0 +1,62 @@
+9#1.Display natural numbers
+'''
+for i in range(1,11):
+    print(i)
+'''
+
+#2.Sum of 10 numbers
+'''
+Sum_of_numbers=0
+for i in range(1,11):
+    Sum_of_numbers += i
+print("The sum of first 10 natural number is:",Sum_of_numbers)
+'''
+#3.print the sum of n no.of terms
+
+n=int(input("Enter the number:"))
+Sum_of_numbers=0
+print("The first",n,"natural numbers are:")
+for i in range(1,n+1):
+    
+    Sum_of_numbers+=i
+print("The sum of the first",n,"natural number are:",Sum_of_numbers)
+
+#4.
+
+
+p=0
+for i in range(1,11):
+    p+=i
+    print("Number - ",i,p)
+average=p/10
+print("The average of the number:",average)
+
+
+#5.
+'''
+n=int(input("Enter an integer:"))
+for i in range(1,n+1):
+    print("Cube of numbers up to",n,"is",i**3)
+'''
+
+#6.
+'''
+number=int(input("Enter the number:"))
+for i in range(1,21):
+    print(number,"x",i,"=",number*i)
+'''
+
+#7.
+
+
+    
+
+
+
+    
+
+
+
+
+
+    
